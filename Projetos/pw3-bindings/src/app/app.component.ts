43cd4cc6ccf0cf40731d97350b12a536e0895799
jjmonstro <jpcorreia1211@gmail.com>
2025-02-18 11:10:00 -0300

    bindings in general and creating new modules

--- Projetos/pw3-bindings/src/app/app.component.ts
+++ Projetos/pw3-bindings/src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InterpolationComponent } from "./interpolation/interpolation.component";
+import { EventBindingComponent } from "./event-binding/event-binding.component";
+import { PropertyBindingComponent } from "./property-binding/property-binding.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InterpolationComponent, EventBindingComponent, PropertyBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  somar(arg0: number,arg1: number) {
-  return arg0+arg1;
-  }
 
-  title = 'Hello word';
+
 }
 
