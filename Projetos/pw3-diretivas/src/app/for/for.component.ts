3292302ba38137471441cdf27e5130f651eb791c
jjmonstro <jpcorreia1211@gmail.com>
2025-05-06 20:30:03 -0300

    its playing sounds on pw3-diretivas

--- Projetos/pw3-diretivas/src/app/for/for.component.ts
+++ Projetos/pw3-diretivas/src/app/for/for.component.ts
@@ -27,13 +27,26 @@ export class ForComponent {
     inputElement.value = '';
   }
 
+  private audio: HTMLAudioElement;
+  constructor() {
+    this.audio = new Audio();
+    this.audio.src = "sounds/fart.mp3";
+    this.audio.load();
+  }
+
   som() {
     // const audioElement = document.getElementById('myAudio') as HTMLAudioElement;
     // audioElement.play();
-    let audio = new Audio();
-    audio.src = "../assets/fart.mp3";
-    audio.load();
-    audio.play();
-    console.log("executou som")
+    // let audio = new Audio();
+    // audio.src = "assets/fart.mp3";
+    // audio.load();
+    // audio.play();
+    // console.log("executou som")
+    this.audio.currentTime = 0; 
+    this.audio.play().then(() => {
+      console.log("Som tocando!");
+    }).catch((error) => { 
+      console.error("Erro ao tocar o som:", error);
+    })
   }
 }
