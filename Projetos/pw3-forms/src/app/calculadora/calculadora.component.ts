5b0a4e327402abf3a85ee9d1bfd5dbdf04a9f7fb
jjmonstro <jpcorreia1211@gmail.com>
2025-05-13 11:08:17 -0300

    forms activity

--- Projetos/pw3-forms/src/app/calculadora/calculadora.component.ts
+++ Projetos/pw3-forms/src/app/calculadora/calculadora.component.ts
@@ -42,18 +42,32 @@ export class CalculadoraComponent {
     return
   }
 
+  private audio: HTMLAudioElement;
   calculadoraForm : FormGroup;
   constructor(private fb: FormBuilder) {
+
     this.calculadoraForm = this.fb.group({
       num1 : this.fb.control(0),
       num2 : this.fb.control(0),
       operacao : this.fb.control('')
     });
+
+    this.audio = new Audio();
+    this.audio.src = "assets/fart.mp3";
+    this.audio.load();
   }
 
 
 
   calcular(){
+
+    this.audio.currentTime = 0;
+    this.audio.play().then(() => {
+      console.log("Som tocando!");
+    }).catch((error) => {
+      console.error("Erro ao tocar o som:", error);
+    })
+
     let num1 = this.calculadoraForm.get('num1')?.value;
     let num2 = this.calculadoraForm.get('num2')?.value
     let operacao = this.calculadoraForm.get('operacao')?.value
--- Projetos/pw3-login/src/app/app.component.ts
+++ Projetos/pw3-login/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CalculadoraComponent } from "./calculadora/calculadora.component";
 import { CadastroComponent } from "./cadastro/cadastro.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CalculadoraComponent, CadastroComponent],
+  imports: [RouterOutlet, CadastroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pw3-forms';
+  title = 'pw3-login';
 }
