8585cab0a83d36442155ca9e445a56268d748b43
Etec Prof. Horácio Augusto da Silveira <jpcorreia1211@gmail.com>
2025-06-10 12:38:03 -0300

    updating last project

--- Projetos/projetinho2bim/src/app/app.component.ts
+++ Projetos/projetinho2bim/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
+import { FuncionalidadeComponent } from "./funcionalidade/funcionalidade.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, FuncionalidadeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Projetos/projetinho2bim/src/app/home/home.component.ts
+++ Projetos/projetinho2bim/src/app/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { FuncionalidadeComponent } from "../funcionalidade/funcionalidade.component";
+import { ContatoComponent } from "../contato/contato.component";
+import { SobreComponent } from "../sobre/sobre.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [FuncionalidadeComponent, ContatoComponent, SobreComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
