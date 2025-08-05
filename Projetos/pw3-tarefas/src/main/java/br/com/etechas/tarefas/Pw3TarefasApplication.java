f9a376948219581c883329f77b31cf52abe663b6
jjmonstro <jpcorreia1211@gmail.com>
2025-08-05 10:58:17 -0300

    pwTarefas feito

---
+++ Projetos/pw3-tarefas/src/main/java/br/com/etechas/tarefas/Pw3TarefasApplication.java
@@ -0,0 +1,14 @@
+package br.com.etechas.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+public class Pw3TarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pw3TarefasApplication.class, args);
+	}
+
+}
