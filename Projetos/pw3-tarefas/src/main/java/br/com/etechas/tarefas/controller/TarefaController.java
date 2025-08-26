6f2709999a9c845ac38b4b39fb4ed09da3beaf38
jjmonstro <jpcorreia1211@gmail.com>
2025-08-26 10:56:22 -0300

    feat: delete method

--- Projetos/pw3-tarefas/src/main/java/br/com/etechas/tarefas/controller/TarefaController.java
+++ Projetos/pw3-tarefas/src/main/java/br/com/etechas/tarefas/controller/TarefaController.java
@@ -1,12 +1,10 @@
 package br.com.etechas.tarefas.controller;
-
+//Matheus Bernardino  João Pedro
 import br.com.etechas.tarefas.dto.TarefaResponseDTO;
-import br.com.etechas.tarefas.entity.Tarefa;
 import br.com.etechas.tarefas.service.TarefaService;
+import com.sun.tools.jconsole.JConsoleContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,13 @@ public class TarefaController {
     @GetMapping
     public List<TarefaResponseDTO> listar(){
         return service.findAll();
+    }
+
+
+    //Matheus Bernardino  João Pedro
+    @DeleteMapping("/{id}")
+    public Object deletar(@PathVariable("id") Long id ){
+        System.out.println(id);
+        return service.excluirPorId(id);
     }
 }
--- Projetos/pw3-tarefas/src/main/java/br/com/etechas/tarefas/service/TarefaService.java
+++ Projetos/pw3-tarefas/src/main/java/br/com/etechas/tarefas/service/TarefaService.java
@@ -2,9 +2,11 @@ package br.com.etechas.tarefas.service;
 
 import br.com.etechas.tarefas.dto.TarefaResponseDTO;
 import br.com.etechas.tarefas.entity.Tarefa;
+import br.com.etechas.tarefas.enums.StatusEnum;
 import br.com.etechas.tarefas.mapper.TarefaMapper;
 import br.com.etechas.tarefas.repository.TarefaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,5 +22,18 @@ public class TarefaService {
 
     public List<TarefaResponseDTO> findAll(){
         return mapper.toResponseDTOList(repository.findAll());
+    }
+
+    public Object excluirPorId(Long id ){
+        if (repository.findById(id) == null){
+            return ResponseEntity.notFound();
+        }
+        Tarefa tarefa = repository.findById(id).get();
+        if (tarefa.getStatus()!=StatusEnum.PENDING){
+            throw new RuntimeException("Tarefa em progresso ou concluida");
+        }
+
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
